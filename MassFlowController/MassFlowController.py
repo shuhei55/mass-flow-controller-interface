0a647068400efb5aa0969254206e283269c29a67
TKshuhei <takamatushuhei@outlook.jp>
2022-12-26 16:54:40 +0900

    finished

--- MassFlowController/MassFlowController.py
+++ MassFlowController/MassFlowController.py
@@ -2,17 +2,44 @@ import threading
 
 import serial
 
+class SerialDummy:
+  def __init__(self):
+    print("Dummy Serial is running!!!!!")
+  
+  def __del__(self):
+    print("Dummy Serial stoped !!!!!")
 
+  def reset_input_buffer(self):
+    pass
+
+  def reset_output_buffer(self):
+    pass
+
+  def write(self, data):
+    pass
+
+  def readline(self):
+    return b"1234567890"
+  
 class MFC:
-
-  def __init__(self, port, baudrate):
-    self.m_serial = serial.Serial(port, baudrate)
-    self.is_communicate = False
-    self.current_flow_list = [0, 0, 0]
-    self.target_flow_list = [0, 0, 0]
-    self.MAX_NUM = 255
-    self.MIN_NUM = 0
-    self.run_communication()
+  def __init__(self, port, baudrate, dummy=False):
+    print("MFC start")
+    if not dummy:
+      self.m_serial = serial.Serial(port, baudrate)
+      self.is_communicate = False
+      self.is_updated = True
+      self.current_flow_list = [0, 0, 0]
+      self.target_flow_list = [0, 0, 0]
+      self.MAX_NUM = 255
+      self.MIN_NUM = 0
+    else:
+      self.m_serial = SerialDummy()
+      self.is_communicate = False
+      self.is_updated = True
+      self.current_flow_list = [0, 0, 0]
+      self.target_flow_list = [0, 0, 0]
+      self.MAX_NUM = 255
+      self.MIN_NUM = 0
 
   def __del__(self):
     del self.m_serial
@@ -21,6 +48,7 @@ class MFC:
     for i in range(3):
       self.target_flow_list[i] = clamp(target_list[i], self.MIN_NUM,
                                        self.MAX_NUM)
+    self.is_updated = True
 
   def get_target(self):
     return self.target_flow_list
@@ -33,10 +61,12 @@ class MFC:
     while self.is_communicate:
       self.m_serial.reset_input_buffer()
       self.m_serial.reset_output_buffer()
-      self.m_serial.write(
-          bytes(
-              str(f"{int(self.target_flow_list[0]):03}{int(self.target_flow_list[1]):03}{int(self.target_flow_list[2]):03}"
-                 ).encode()))
+      if self.is_updated:
+        self.m_serial.write(
+            bytes(
+                str(f"{int(self.target_flow_list[0]):03}{int(self.target_flow_list[1]):03}{int(self.target_flow_list[2]):03}"
+                  ).encode()))
+        self.is_updated = False
       data = self.m_serial.readline()
       self.current_flow_list[0] = int(data[:3])
       self.current_flow_list[1] = int(data[3:6])
